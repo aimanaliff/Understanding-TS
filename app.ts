f997a7d7faf0f210feabac30fae296cec3b45077
Aliff Aiman <aliffaimanridzuan@gmail.com>
2020-12-10 19:36:29 +0800

    test

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string){
 const number1 = 5; //5.0
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: ';
+const resultPhrase = 'Test ';
 
 add(number1, number2, printResult, resultPhrase);
 
