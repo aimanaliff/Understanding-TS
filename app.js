8a4636383eec31db86319259482a05ca83056642
Aliff Aiman <aliffaimanridzuan@gmail.com>
2021-01-20 00:49:26 +0800

    run through all core types

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]; //tuple: fixed-length array with fixed type
+// } = {
+//     name: 'Aliff',
+//     age: 21,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
-    // const person = {
     name: 'Aliff',
-    age: 21
+    age: 21,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
+// person.role.push('admin'); //.push is an exception in ts
+// person.role[1] = 10; //assigned wrong value
+// person.role = [0, 'admin', 'user']; //error because fixed array length
+var favouriteactivities;
+favouriteactivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
